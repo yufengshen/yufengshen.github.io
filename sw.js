877f7504933c3ef962fa9e03448d260307337d54
Yufeng Shen <yufeng.sy@alibaba-inc.com>
2016-06-02 16:08:39 +0800

    show

--- sw.js
+++ sw.js
@@ -8,6 +8,13 @@ self.addEventListener('activate', function(event) {
 });
 self.addEventListener('push', function(event) {
     console.log('Push message received', event);
+    var title = 'Push message';
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: 'The Message',
+            icon: 'images/icon.png',
+            tag: 'my-tag'
+        }));
   // TODO
 });
 
