67fef8c7a105a5163e3a896bc38c59a5bd0362db
Yufeng Shen <yufeng.sy@alibaba-inc.com>
2016-06-02 16:06:04 +0800

    click

--- sw.js
+++ sw.js
@@ -10,3 +10,27 @@ self.addEventListener('push', function(event) {
     console.log('Push message received', event);
   // TODO
 });
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('Notification click: tag ', event.notification.tag);
+    event.notification.close();
+    var url = 'https://youtu.be/gYMkEMCHtJ4';
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+            .then(function(windowClients) {
+                for (var i = 0; i < windowClients.length; i++) {
+                    var client = windowClients[i];
+                    if (client.url === url && 'focus' in client) {
+                        return client.focus();
+                    }
+                }
+                if (clients.openWindow) {
+                    return clients.openWindow(url);
+                }
+            })
+    );
+});
+
+
